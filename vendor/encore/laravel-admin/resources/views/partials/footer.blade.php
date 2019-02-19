0df39d8391255034793128c70eca0d9e0c1204ba
zhuzejun <zhuzj@bestzst.com>
2019-02-19 15:42:40 +0800

    'b'

--- vendor/encore/laravel-admin/resources/views/partials/footer.blade.php
+++ vendor/encore/laravel-admin/resources/views/partials/footer.blade.php
@@ -14,5 +14,5 @@
 
     </div>
     <!-- Default to the left -->
-    <strong>Powered by <a href="https://github.com/z-song/laravel-admin" target="_blank">laravel-admin</a></strong>
+    <strong>技术支持：诸泽俊 16601718803</strong>
 </footer>
